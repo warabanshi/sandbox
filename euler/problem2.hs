6126ecda92e8e4d943ad8ae29a33bfd38d8e3448
warabanshi <warabanshi@gmail.com>
2014-05-04 11:51:51 +0900

    remove euler

--- euler/problem2.hs
+++
@@ -1,7 +0,0 @@
-import Data.List
-
-fib :: Int -> Int -> Int -> Int
-fib m n x
-    | n > 4000000 = x
-    | even n = fib n (m+n) (x+n)
-    | otherwise = fib n (m+n) x
--- euler/problem3.hs
+++
@@ -1,7 +0,0 @@
-import Data.List
-
-intFactorization :: Int -> Int -> Int
-intFactorization n divisor
-    | n == 1 = divisor
-    | n `mod` divisor == 0 = intFactorization (n `div` divisor) divisor
-    | otherwise = intFactorization n (divisor+1)
