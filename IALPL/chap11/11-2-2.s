a8a1bdb92184fb66578bbd822b935af8e0db0668
warabanshi <warabanshi@gmail.com>
2014-05-04 20:52:04 +0900

    add movaps, movups

--- IALPL/chap11/11-2-2.s
+++ IALPL/chap11/11-2-2.s
@@ -1,13 +1,15 @@
     segment .data
 a   resd    12
 b   resw    11
-    align   16
-c   resq    13
+;    align   16
+c   resq    2
 
     segment .text
     global  main
 main:
     push    rbp
     mov     rbp, rsp
+    movaps  xmm0, [c]
+;   movups  xmm0, [c]
     leave
     ret
